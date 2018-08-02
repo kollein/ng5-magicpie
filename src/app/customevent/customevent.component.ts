5ccf356c83a61add28be89448248f8710389dcf2
kollein <kolleinvinh1993@gmail.com>
2018-08-02 12:32:01 +0700

    update

--- src/app/customevent/customevent.component.ts
+++ src/app/customevent/customevent.component.ts
@@ -92,15 +92,23 @@ export class CustomeventComponent implements OnInit {
   restartFlashView() {
     let opts = {
       data_container: "data-flash-view",
+      data_flash_view_img: "data-flash-view-img",
       view_modal: "view-modal",
       view_modal_img: "view-modal-img",
+      on_drag_cls: "on-drag",
+      on_loose_cls: "on-loose",
       is_zooming_cls: "is-zooming",
       is_zoomed_cls: "is-zoomed",
+      is_zoomingout_cls: "is-zoomingout",
+      is_zoomedout_cls: "is-zoomedout",
       mouseEvents: [this.eventList.mouseup, this.eventList.mouseleave],
       cheatKeys: {
         happyEnding: 'happy-ending'
-      }
+      },
+      dragDistanceToDismiss: 5
     }
+    let transform_is_zooming;
+    let body_el = this.d.querySelector('body');
 
     this.d.addEventListener(this.eventList.mousedown, (event) => {
 
@@ -113,21 +121,19 @@ export class CustomeventComponent implements OnInit {
 
       let real_target = event.target;
       let container_target = this.getClosestTargetByAttrName(real_target, opts.data_container);
-      let body_el = this.d.querySelector('body');
 
       if ( container_target !== null ) {
 
+        let data_flash_view_img_el = container_target.querySelector(`[${opts.data_flash_view_img}]`);
         let view_modal_el = container_target.querySelector(`.${opts.view_modal}`);
         let view_modal_img_el = view_modal_el.querySelector(`.${opts.view_modal_img}`);
         // cached for global using
         let modalFullWidth = 0,
-            modalFullHeight = 0,
-            transform_is_zooming;
+            modalFullHeight = 0;
 
         let removeEffect = () => {
-          console.log('removeEffect');
+
           view_modal_el.removeEventListener("transitionend", removeEffect);
-          
           // make it smoothly on animation state
           setTimeout(() => {
             view_modal_el.classList.remove(opts.is_zooming_cls);
@@ -139,15 +145,18 @@ export class CustomeventComponent implements OnInit {
 
         let changeMisc = (state) => {
           let overflowVal = 'hidden',
-              attrVal = 'on';
+              attrVal = 'on',
+              imgVisibility = 'hidden';
 
           if ( state === 'off' ) {
             overflowVal = 'static',
             attrVal = 'off';
+            imgVisibility = 'visible';
           }
 
           body_el.style.cssText = `overflow: ${overflowVal}`;
           container_target.setAttribute(opts.data_container, attrVal);
+          // data_flash_view_img_el.style.cssText = `visibility: ${imgVisibility}`;
         }
 
         if ( container_target.getAttribute(opts.data_container) !== 'on' ) {
@@ -183,7 +192,8 @@ export class CustomeventComponent implements OnInit {
           // console.log('translateLeft: ', translateLeft, 'translateTop: ', translateTop);
 
           view_modal_el.classList.add(opts.is_zooming_cls);
-          view_modal_el.style.cssText = `transform: translate(${translateLeft}px, ${translateTop}px) scale(${ratio});`;
+          transform_is_zooming = `transform: translate(${translateLeft}px, ${translateTop}px) scale(${ratio});`;
+          view_modal_el.style.cssText = transform_is_zooming;
 
           view_modal_el.addEventListener("transitionend", removeEffect);
         }
@@ -191,7 +201,7 @@ export class CustomeventComponent implements OnInit {
 
         // Dragging Image
         if ( real_target.classList.contains(opts.view_modal_img) ) {
-
+          
           let client = <any>this.getClientXY(event);
           let timer_for_dismiss;
 
@@ -211,8 +221,7 @@ export class CustomeventComponent implements OnInit {
                 prevTopPos = prevTopPos ? prevTopPos : 0;
             // console.log(prevLeftPos, 'inpx');
             // console.log(prevTopPos, 'inpx');
-            transform_is_zooming = `translate(${(distanceX + prevLeftPos)}px, ${(distanceY + prevTopPos)}px)`;
-            real_target.style.transform = transform_is_zooming;
+            real_target.style.transform = `translate(${(distanceX + prevLeftPos)}px, ${(distanceY + prevTopPos)}px)`;
 
             // swipe-down to dissmiss modal
             clearTimeout(timer_for_dismiss);
@@ -221,26 +230,53 @@ export class CustomeventComponent implements OnInit {
               console.log(get_mouse_state(), opts.mouseEvents.indexOf(get_mouse_state()));
               
               if ( opts.mouseEvents.indexOf(get_mouse_state()) > -1 ) {
-                Math.abs(distanceY) > 15 && endtour(opts.cheatKeys.happyEnding);
+                Math.abs(distanceY) > opts.dragDistanceToDismiss && endtour(opts.cheatKeys.happyEnding);
               }
             }, 100);
           }
 
+          let removeZoomingoutEffect = () => {
+            view_modal_el.classList.remove(opts.is_zoomingout_cls, opts.is_zoomedout_cls);
+            view_modal_el.style.cssText = '';
+            view_modal_el.removeEventListener("transitionend", removeZoomingoutEffect);
+            changeMisc('off');
+          }
+          
           let endtour = (state) => {
             // update: state = event
             _mouse_state = state.type;
 
             if ( state === opts.cheatKeys.happyEnding ) {
+
               view_modal_el.classList.remove(opts.is_zoomed_cls);
-              view_modal_el.classList.add(opts.is_zooming_cls);
-              real_target.style.cssText = `transition: transform .3s;transform: translate(0px, 0px) scale(1);`;
-              changeMisc('off');
-            } else {
-              real_target.style.transition = `transform .12s linear`;
-              real_target.style.transform = `translate(0px, 0px)`;
+              real_target.style.cssText = '';
+              view_modal_el.classList.add(opts.is_zoomingout_cls);
+              view_modal_el.style.cssText = transform_is_zooming;
+
               setTimeout(() => {
-                real_target.style.transition = '';
-              }, 120);
+                view_modal_el.classList.remove(opts.is_zoomingout_cls);
+                view_modal_el.style.cssText = '';
+                view_modal_el.classList.add(opts.is_zoomedout_cls);
+                view_modal_el.addEventListener("transitionend", removeZoomingoutEffect);
+              }, 50);
+
+            } else {
+              let removeOnDragEffect = () => {
+                real_target.classList.remove(opts.on_drag_cls);
+                real_target.classList.add(opts.on_loose_cls);
+                real_target.removeEventListener("transitionend", removeOnDragEffect);
+              }
+              let removeOnLooseEffect = () => {
+                real_target.classList.remove(opts.on_loose_cls);
+                real_target.removeEventListener("animationend", removeOnLooseEffect);
+              }
+
+              real_target.style.transform = '';
+              real_target.style.trasition = '';
+              real_target.classList.add(opts.on_drag_cls);
+              real_target.addEventListener("transitionend", removeOnDragEffect);
+              real_target.addEventListener("animationend", removeOnLooseEffect);
+
             }
             
             real_target.removeEventListener(this.eventList.mousemove, movingByDragging);
